66da66c597c81ce1c43a78d4e3745c23198b4d05
P. S. Clarke <firefox.base@pscl4rke.net>
2015-02-25 23:16:13 +0000

    Add dark browsing extension.

--- profile/prefs.js
+++ profile/prefs.js
@@ -9,6 +9,7 @@
  */
 
 user_pref("app.update.lastUpdateTime.browser-cleanup-thumbnails", 0);
+user_pref("browser.anchor_color", "#4466FF");
 user_pref("browser.bookmarks.restore_default_bookmarks", false);
 user_pref("browser.cache.disk.capacity", 358400);
 user_pref("browser.cache.disk.smart_size.first_run", false);
@@ -22,14 +23,18 @@ user_pref("browser.places.smartBookmarksVersion", 7);
 user_pref("browser.sessionstore.upgradeBackup.latestBuildID", "20150114034423");
 user_pref("browser.slowStartup.averageTime", 10959);
 user_pref("browser.slowStartup.samples", 1);
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"PanelUI-contents\":[\"edit-controls\",\"zoom-controls\",\"new-window-button\",\"privatebrowsing-button\",\"save-page-button\",\"print-button\",\"history-panelmenu\",\"fullscreen-button\",\"find-button\",\"preferences-button\",\"add-ons-button\",\"developer-button\"],\"addon-bar\":[\"addonbar-closebutton\",\"status-bar\"],\"PersonalToolbar\":[\"personal-bookmarks\"],\"nav-bar\":[\"urlbar-container\",\"search-container\",\"webrtc-status-button\",\"bookmarks-menu-button\",\"downloads-button\",\"home-button\",\"social-share-button\",\"widget:jid1-AGzBN6dsbJWp3A@jetpack-ReverseColors\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"toolbar-menubar\":[\"menubar-items\"]},\"seen\":[],\"dirtyAreaCache\":[\"PersonalToolbar\",\"nav-bar\",\"TabsToolbar\",\"toolbar-menubar\"],\"newElementCount\":0}");
+user_pref("browser.visited_color", "#991AFF");
 user_pref("extensions.blocklist.pingCountVersion", 0);
-user_pref("extensions.bootstrappedAddons", "{\"langpack-en-GB@iceweasel.mozilla.org\":{\"version\":\"31.4.0\",\"type\":\"locale\",\"descriptor\":\"/usr/lib/iceweasel/browser/extensions/langpack-en-GB@iceweasel.mozilla.org.xpi\"},\"VimFx@akhodakivskiy.github.com\":{\"version\":\"0.5.17\",\"type\":\"extension\",\"descriptor\":\"/home/psc/firefox.base/profile/extensions/VimFx@akhodakivskiy.github.com.xpi\"}}");
+user_pref("extensions.bootstrappedAddons", "{\"VimFx@akhodakivskiy.github.com\":{\"version\":\"0.5.17\",\"type\":\"extension\",\"descriptor\":\"/home/psc/firefox.base/profile/extensions/VimFx@akhodakivskiy.github.com.xpi\"},\"langpack-en-GB@iceweasel.mozilla.org\":{\"version\":\"31.4.0\",\"type\":\"locale\",\"descriptor\":\"/usr/lib/iceweasel/browser/extensions/langpack-en-GB@iceweasel.mozilla.org.xpi\"},\"jid1-AGzBN6dsbJWp3A@jetpack\":{\"version\":\"0.3.2\",\"type\":\"extension\",\"descriptor\":\"/home/psc/firefox.base/profile/extensions/jid1-AGzBN6dsbJWp3A@jetpack.xpi\"}}");
 user_pref("extensions.databaseSchema", 16);
 user_pref("extensions.enabledAddons", "%7B972ce4c6-7e08-4474-a285-3208198ce6fd%7D:31.4.0");
-user_pref("extensions.installCache", "[{\"name\":\"app-global\",\"addons\":{\"{972ce4c6-7e08-4474-a285-3208198ce6fd}\":{\"descriptor\":\"/usr/lib/iceweasel/browser/extensions/{972ce4c6-7e08-4474-a285-3208198ce6fd}\",\"mtime\":1421507664000,\"rdfTime\":1421208721000},\"langpack-en-GB@iceweasel.mozilla.org\":{\"descriptor\":\"/usr/lib/iceweasel/browser/extensions/langpack-en-GB@iceweasel.mozilla.org.xpi\",\"mtime\":1421147954000}}}]");
+user_pref("extensions.getAddons.databaseSchema", 5);
+user_pref("extensions.installCache", "[{\"name\":\"app-global\",\"addons\":{\"{972ce4c6-7e08-4474-a285-3208198ce6fd}\":{\"descriptor\":\"/usr/lib/iceweasel/browser/extensions/{972ce4c6-7e08-4474-a285-3208198ce6fd}\",\"mtime\":1421507664000,\"rdfTime\":1421208721000},\"langpack-en-GB@iceweasel.mozilla.org\":{\"descriptor\":\"/usr/lib/iceweasel/browser/extensions/langpack-en-GB@iceweasel.mozilla.org.xpi\",\"mtime\":1421147954000}}},{\"name\":\"app-profile\",\"addons\":{\"VimFx@akhodakivskiy.github.com\":{\"descriptor\":\"/home/psc/firefox.base/profile/extensions/VimFx@akhodakivskiy.github.com.xpi\",\"mtime\":1424891378000}}}]");
 user_pref("extensions.lastAppVersion", "31.4.0");
 user_pref("extensions.lastPlatformVersion", "31.4.0");
 user_pref("extensions.pendingOperations", false);
+user_pref("extensions.sdk-widget-inserted.widget:jid1-AGzBN6dsbJWp3A@jetpack-ReverseColors", true);
 user_pref("extensions.shownSelectionUI", true);
 user_pref("gecko.buildID", "20150114034423");
 user_pref("gecko.mstone", "31.4.0");
